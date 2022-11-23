fc2b1090f412e753002d42568b97435bb660bba5
Turjo99 <imturjo27@gmail.com>
2022-11-24 01:43:34 +0600

    Categories API Called from database

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xyxb77f.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+async function run() {
+  try {
+    const categoryCollection = client.db("jossCar").collection("categories");
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoryCollection.find(query).toArray();
+      res.send(categories);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 app.get("/", (req, res) => {
   res.send("Joss Car Server Running");
 });
