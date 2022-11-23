a21726bfe7a8d5d4872ec6f72bd1668ed0dafcb8
Turjo99 <imturjo27@gmail.com>
2022-11-24 01:19:49 +0600

    Joss Car Server Created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Joss Car Server Running");
+});
+
+app.listen(port, () => {
+  console.log("node running");
+});
